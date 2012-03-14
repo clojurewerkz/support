37179e52791cac032f80265f7ef435f85d95ac32
Michael S. Klishin <michael@novemberain.com>
2012-03-14 23:12:14 +0400

    Add Guava dependency so that other projects don't have to do it

--- project.clj
+++ project.clj
@@ -4,9 +4,10 @@
   :url "http://github.com/clojurewerkz/support"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.3.0"]]
-  :profiles { :all { :resource-paths ["test/resources"] }
-             :1.4 { :dependencies [[org.clojure/clojure "1.4.0-beta4"]] } }
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [com.google.guava/guava "11.0.1"]]
+  :profiles { :dev { :resource-paths ["test/resources"] }
+              :1.4 { :dependencies [[org.clojure/clojure "1.4.0-beta4"]] } }
   :aliases { "all" ["with-profile" "dev:dev,1.4"] }
   :source-paths ["src/clojure"]  
   :java-source-paths ["src/java"]  
