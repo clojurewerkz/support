830851b7b46e99aab8ecac61cd2baa64b9265f41
Michael S. Klishin <michael@novemberain.com>
2012-04-15 20:15:19 +0400

    Clojure 1.4.0

--- README.md
+++ README.md
@@ -78,7 +78,8 @@ CI is hosted by [travis-ci.org](http://travis-ci.org)
 
 ## Development
 
-Neocons uses [Leiningen 2](https://github.com/technomancy/leiningen/blob/master/doc/TUTORIAL.md). Make sure you have it installed and then run tests against Clojure 1.3.0 and 1.4.0[-beta4] using
+Neocons uses [Leiningen 2](https://github.com/technomancy/leiningen/blob/master/doc/TUTORIAL.md). Make sure you have it installed and then run tests
+against all supported Clojure versions using
 
     lein2 all test
 
--- project.clj
+++ project.clj
@@ -7,7 +7,11 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [com.google.guava/guava "11.0.1"]]
   :profiles { :dev { :resource-paths ["test/resources"] }
-              :1.4 { :dependencies [[org.clojure/clojure "1.4.0-beta6"]] } }
+             :1.4 { :dependencies [[org.clojure/clojure "1.4.0"]] } }
+  :repositories {"clojure-releases" "http://build.clojure.org/releases"
+                 "sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
+                             :snapshots false
+                             :releases {:checksum :fail :update :always}}}  
   :aliases { "all" ["with-profile" "dev:dev,1.4"] }
   :source-paths ["src/clojure"]  
   :java-source-paths ["src/java"]  
