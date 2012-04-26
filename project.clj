fad5f12d9cd51398a0c7f5ed83f3dfab2f065a97
Michael S. Klishin <michael@novemberain.com>
2012-04-26 20:56:10 +0400

    Extract clojure.data.json/Joda Time integration from Monger

--- project.clj
+++ project.clj
@@ -1,18 +1,24 @@
 (defproject clojurewerkz/support "0.2.0-SNAPSHOT"
   :description "A support library ClojureWerkz projects (Langohr, Monger, Neocons, Elastisch, Quartzite, Money, etc) can rely on"
-  :min-lein-version "2.0.0"  
+  :min-lein-version "2.0.0"
   :url "http://github.com/clojurewerkz/support"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [com.google.guava/guava "11.0.1"]]
-  :profiles { :dev { :resource-paths ["test/resources"] }
-             :1.4 { :dependencies [[org.clojure/clojure "1.4.0"]] } }
-  :repositories {"clojure-releases" "http://build.clojure.org/releases"
-                 "sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
+  :profiles {:dev {:resource-paths ["test/resources"]
+                   :dependencies   [[org.clojure/data.json "0.1.2"]
+                                    [clj-time              "0.4.2"]]}
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}}
+  :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                              :snapshots false
-                             :releases {:checksum :fail :update :always}}}  
-  :aliases { "all" ["with-profile" "dev:dev,1.4"] }
-  :source-paths ["src/clojure"]  
-  :java-source-paths ["src/java"]  
+                             :releases {:checksum :fail :update :always}}
+                 "sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
+                             :snapshots true
+                             :releases {:checksum :fail :update :always}}}
+  :aliases {"all" ["with-profile" "dev:dev,1.4:dev,1.5"]
+            "ci"  ["with-profile" "dev:dev,1.4"]}
+  :source-paths ["src/clojure"]
+  :java-source-paths ["src/java"]
   :warn-on-reflection true)
---
+++ test/clojurewerkz/support/json_test.clj
@@ -0,0 +1,9 @@
+(ns clojurewerkz.support.json-test
+  (:use clojure.test
+        clojurewerkz.support.json)
+  (:require [clojure.data.json :as json]
+            [clj-time.core     :as t]))
+
+
+(deftest serialization-of-joda-datetime-to-json-with-clojure-data-json
+  (is (= "\"2011-10-13T23:55:00.000Z\"" (json/json-str (t/date-time 2011 10 13 23 55 0)))))
