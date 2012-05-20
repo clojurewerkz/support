e639641e16d6ba635c14b3bc740443bea184a09c
Michael S. Klishin <michael@novemberain.com>
2012-05-21 02:54:06 +0400

    Spam

--- test/clojurewerkz/support/re_test.clj
+++ test/clojurewerkz/support/re_test.clj
@@ -3,7 +3,7 @@
   (:use clojure.test))
 
 
-(deftest ^{:focus true} test-re-gsub
+(deftest test-re-gsub
   (are [re replacement s result] (is (= result (re/re-gsub re replacement s)))
        #"world" "Clojure" "Hello, world" "Hello, Clojure"
        #"_id$" "" "id_field_id" "id_field"))
