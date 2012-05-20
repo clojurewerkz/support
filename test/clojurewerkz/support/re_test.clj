ea4911953b34cc67601caef9c57a013e575bc8ce
Michael S. Klishin <michael@novemberain.com>
2012-05-21 03:39:30 +0400

    More clojurewerkz.support.re work

--- test/clojurewerkz/support/re_test.clj
+++ test/clojurewerkz/support/re_test.clj
@@ -6,4 +6,5 @@
 (deftest test-re-gsub
   (are [re replacement s result] (is (= result (re/re-gsub re replacement s)))
        #"world" "Clojure" "Hello, world" "Hello, Clojure"
-       #"_id$" "" "id_field_id" "id_field"))
+       #"_id$" "" "id_field_id" "id_field"
+       #"abc-(\d+)-def" "abc-$1" "abc-883-def" "abc-883"))
