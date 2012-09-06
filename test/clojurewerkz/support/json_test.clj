bef4258396da2532c87fd279f503ae6ecac4d7d8
Michael S. Klishin <michael@novemberain.com>
2012-09-07 00:26:33 +0400

    Correct test names

--- test/clojurewerkz/support/json_test.clj
+++ test/clojurewerkz/support/json_test.clj
@@ -6,13 +6,13 @@
             [clj-time.core     :as t]))
 
 
-(deftest serialization-of-joda-datetime-to-json-with-clojure-data-json
+(deftest serialization-of-joda-datetime-to-json
   (let [dt (t/date-time 2011 10 13 23 55 0)]
     (is (= "\"2011-10-13T23:55:00.000Z\""
            (json/json-str dt)
            (json2/encode dt)))))
 
-(deftest serialization-of-java-util-date-to-json-with-clojure-data-json
+(deftest serialization-of-java-util-date-to-json
   (let [d (.toDate (t/date-time 2011 10 13 23 55 0))]
     (is (= "\"2011-10-13T23:55:00.000Z\""
            (json/json-str d)))
