c4223ff2b01b0a5190aabb93a7cc41fa365624ff
Michael Klishin <michael@defprotocol.org>
2013-03-16 02:17:33 +0400

    clj-time 0.4.5

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.google.guava/guava "14.0.1"]]
   :profiles {:dev {:resource-paths ["test/resources"]
-                   :dependencies   [[clj-time              "0.4.4"]
+                   :dependencies   [[clj-time              "0.4.5"]
                                     [cheshire              "5.0.2"]]}
              :1.3    {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.5    {:dependencies [[org.clojure/clojure "1.5.0-RC16"]]}
