e8961cf5f48ed3a6862fc45650ff547c0488a5f3
Michael Klishin <michael@defprotocol.org>
2013-09-12 22:57:45 +0100

    Introduce c.s.internal/char-array-type

--- src/clojure/clojurewerkz/support/internal.clj
+++ src/clojure/clojurewerkz/support/internal.clj
@@ -1,5 +1,9 @@
-(ns clojurewerkz.support.internal)
+(ns clojurewerkz.support.internal
+  "Various low-level plumbing bits")
 
 ;; clojure.java.io has these as private, so we had to copy them. MK.
 (def ^{:doc "Type object for a Java primitive byte array."}
   byte-array-type (class (make-array Byte/TYPE 0)))
+
+(def ^{:doc "Type object for a Java primitive char array."}
+  char-array-type (class (make-array Character/TYPE 0)))
