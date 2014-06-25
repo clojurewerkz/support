d71cd594b297ff62038a41665069777b3b19273b
Michael Klishin <michael@clojurewerkz.org>
2014-06-25 22:06:50 +0400

    Depend on 1.6.0

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :url "http://github.com/clojurewerkz/support"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.google.guava/guava "17.0"]]
   :profiles {:dev {:resource-paths ["test/resources"]
                    :dependencies   [[clj-time              "0.6.0"]
